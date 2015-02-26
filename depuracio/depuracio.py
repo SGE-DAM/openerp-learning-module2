da1f5f29670a290d1c3f7ac759ab76c4606c872a
xxjcaxx <xxjcaxx@yahoo.es>
2015-02-26 12:22:51 +0100

    dep1

--- depuracio/depuracio.py
+++ depuracio/depuracio.py
@@ -1,14 +1,14 @@
 from osv import osv, fields
 
-class depuracio_deppartner(osv.osv):
-	#_name = 'depuracio.deppartner'
+class depuracio_metodes(osv.osv):
+
 	# https://doc.odoo.com/v6.0/developer/2_5_Objects_Fields_Methods/methods.html/
 	def create(self, cr, uid, values, context=None):
 
-		p_id = super(depuracio_deppartner, self).create(cr, uid, values, context=context)
+		p_id = super(depuracio_metodes, self).create(cr, uid, values, context=context)
 		p = self.browse(cr, uid, p_id, context=context)
 		p_data = self.copy_data(cr, uid, p_id, default=None, context=None)
-		print "Datos del res.partner: "
+		print "Datos: "
 		for i in p_data:
 			descripcions = self.fields_get(cr,uid,i,context=None)
 			print "\t"+str(i)+": "+str(p_data[i])+" - "+str(descripcions)
@@ -17,28 +17,27 @@ class depuracio_deppartner(osv.osv):
 
 	def write(self, cr, uid, ids, values, context=None):
 
-		super(depuracio_deppartner, self).write(cr, uid, ids, values, context=None)
+		super(depuracio_metodes, self).write(cr, uid, ids, values, context=None)
 		p = self.browse(cr, uid, ids, context=context)
 		for j in p:
 			p_data = self.copy_data(cr, uid, j.id, default=None, context=None)
-			print "Datos del res.partner: "
+			print "Datos:"
 			for i in p_data:
 				descripcions = self.fields_get(cr,uid,i,context=None)
 				print "\t"+str(i)+": "+str(p_data[i])+" - "+str(descripcions)
 
 		return True
 
-	_inherit = 'res.partner'
+	_name = 'depuracio.metodes'
 	_columns = {
 
 	}
-depuracio_deppartner()
+depuracio_metodes()
 
-"""class depuracio_depproduct(osv.osv):
-	#_name = 'depuracio.depproduct'
-	_inherit = 'product.product'
+class depuracio_depproduct(osv.osv):
+	_name = 'depuracio.depproduct'
+	_inherit = 'depuracio.metodes'
 	_columns = {
-
+		'name': fields.integer('Name'),
 	}
 depuracio_depproduct()
-"""
